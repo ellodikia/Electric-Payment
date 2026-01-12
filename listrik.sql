fc3589c0cc806805ee041b53618869ec207b0e56
ellodikia <gabriellaodikia@gmail.com>
2026-01-12 21:01:15 +0700

    update 2

--- listrik.sql
+++ listrik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 10, 2026 at 04:19 PM
+-- Generation Time: Jan 11, 2026 at 05:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -56,6 +56,13 @@ CREATE TABLE `payment_pelanggan` (
   `kodetarif` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `payment_pelanggan`
+--
+
+INSERT INTO `payment_pelanggan` (`id`, `id_pelanggan`, `nometer`, `nama`, `alamat`, `kodetarif`) VALUES
+(45, '900990', '890890', 'Gabriell', 'Jl. Karya Jasa', 'R1/900');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +80,14 @@ CREATE TABLE `payment_pembayaran` (
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `payment_pembayaran`
+--
+
+INSERT INTO `payment_pembayaran` (`id`, `id_pelanggan`, `id_bayar`, `tanggal`, `bulanbayar`, `biayaadmin`, `total`, `status`) VALUES
+(1, '900990', 'PAY-1768060751', '2026-01-10', 'Januari', 2500, '270000', 'Lunas'),
+(2, '900990', 'PAY-1768142274', '2026-01-11', 'Februari', 2500, '10935000', 'Lunas');
+
 -- --------------------------------------------------------
 
 --
@@ -87,6 +102,14 @@ CREATE TABLE `payment_penggunaan` (
   `meterawal` int(11) NOT NULL,
   `meterakhir` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment_penggunaan`
+--
+
+INSERT INTO `payment_penggunaan` (`id`, `id_pelanggan`, `bulan`, `tahun`, `meterawal`, `meterakhir`) VALUES
+(22, '900990', 'Januari', '2026', 100, 300),
+(23, '900990', 'Februari', '2026', 900, 9000);
 
 -- --------------------------------------------------------
 
@@ -104,6 +127,14 @@ CREATE TABLE `payment_tagihan` (
   `status` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `payment_tagihan`
+--
+
+INSERT INTO `payment_tagihan` (`id`, `id_pelanggan`, `bulan`, `tahun`, `jumlahmeter`, `jumlahtagihan`, `status`) VALUES
+(22, '900990', 'Januari', '2026', 200, '270000', 1),
+(23, '900990', 'Februari', '2026', 8100, '10935000', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -116,6 +147,13 @@ CREATE TABLE `payment_tarif` (
   `daya` varchar(50) NOT NULL,
   `tarifperkwh` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `payment_tarif`
+--
+
+INSERT INTO `payment_tarif` (`id`, `kodetarif`, `daya`, `tarifperkwh`) VALUES
+(5, 'R1/900', '900', '1350');
 
 --
 -- Indexes for dumped tables
@@ -177,31 +215,31 @@ ALTER TABLE `payment_login`
 -- AUTO_INCREMENT for table `payment_pelanggan`
 --
 ALTER TABLE `payment_pelanggan`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `payment_pembayaran`
 --
 ALTER TABLE `payment_pembayaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `payment_penggunaan`
 --
 ALTER TABLE `payment_penggunaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `payment_tagihan`
 --
 ALTER TABLE `payment_tagihan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `payment_tarif`
 --
 ALTER TABLE `payment_tarif`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
